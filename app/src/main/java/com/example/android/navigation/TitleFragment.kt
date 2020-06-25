1a126b7d7a27ce24ccc78cbbfb540aec166112c1
yrnr <iamramuhere@gmail.com>
2020-06-25 17:15:30 +0530

    Navigation class - onClickListener - ktx style

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
+import java.util.*
 
 class TitleFragment : Fragment() {
 
@@ -14,6 +17,13 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+//        binding.playButton.setOnClickListener(){
+//            view: View -> Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+//              view: View -> view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+//        }
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(
+                    R.id.action_titleFragment_to_gameFragment)  )
         return binding.root
     }
 }
